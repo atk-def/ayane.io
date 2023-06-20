0ad7ceabb78b0e338a427f4a9ee35fc24607e5b6
Ayane <necromancer@minakami.land>
2023-06-20 20:40:37 +0800

    fix: typo and merge animation definitions

--- src/utils/anim.ts
+++ src/utils/anim.ts
@@ -23,44 +23,43 @@ export const toggleAnimationVariants = {
       },
     },
   },
-}
-
-export const menuAnimationVariants = {
-  open: {
-    width: '100%',
-    height: '100%',
-    top: 0,
-    left: 0,
-    opacity: 1,
-    borderRadius: 0,
-    transition: {
-      type: 'spring',
-      restDelta: 2,
-      duration: 0.5,
+  fullScreen: {
+    open: {
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      opacity: 1,
+      borderRadius: 0,
+      transition: {
+        type: 'spring',
+        restDelta: 2,
+        duration: 0.5,
+      },
     },
+    closed: ({
+      width,
+      height,
+      top,
+      left,
+    }: {
+      width: number
+      height: number
+      top: number
+      left: number
+    }) => ({
+      width,
+      height,
+      top,
+      left,
+      opacity: 0,
+      borderRadius: 100,
+      transition: {
+        type: 'spring',
+        stiffness: 400,
+        damping: 40,
+        delay: 0.5,
+      },
+    }),
   },
-  closed: ({
-    width,
-    height,
-    top,
-    left,
-  }: {
-    width: number
-    height: number
-    top: number
-    left: number
-  }) => ({
-    width,
-    height,
-    top,
-    left,
-    opacity: 0,
-    borderRadius: 100,
-    transition: {
-      type: 'spring',
-      stiffness: 400,
-      damping: 40,
-      delay: 0.5,
-    },
-  }),
 }
